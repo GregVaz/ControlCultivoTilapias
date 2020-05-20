51bd4c07392ff97dd0fd684c1f4470ad9e656469
Daniel Gonzalez Placito <daargonzalezpl@ittepic.edu.mx>
2020-05-20 12:56:23 -0600

    Importando ChartJS Y Ejemplo eEn Estadisticas

--- src/app/pages/statistics-page/statistics-page.component.spec.ts
+++ src/app/pages/statistics-page/statistics-page.component.spec.ts
@@ -1,5 +1,4 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { StatisticsPageComponent } from './statistics-page.component';
 
 describe('StatisticsPageComponent', () => {
--- src/app/pages/statistics-page/statistics-page.component.ts
+++ src/app/pages/statistics-page/statistics-page.component.ts
@@ -1,16 +1,55 @@
 import { Component, OnInit } from '@angular/core';
 import { FirebaseService } from 'src/app/services/firebase.service';
-
+import { Chart } from 'chart.js';
 @Component({
   selector: 'app-statistics-page',
   templateUrl: './statistics-page.component.html',
   styleUrls: ['./statistics-page.component.scss']
 })
 export class StatisticsPageComponent implements OnInit {
-
+graficoPastel = [];
   constructor(public fbService: FirebaseService) { }
 
   ngOnInit(): void {
+    this.graficoPastel = new Chart('chartPastel', {
+      type: 'pie',
+      data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+          label: '# of Votes',
+          data: [9, 7, 3, 5, 2, 10],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        title: {
+          text: "Grafica De Pastel",
+          display: true
+        },
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
   }
-
 }
